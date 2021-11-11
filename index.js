a7cddbc7209228810c8116421d6d7081e12f8f6a
Ayush Sharma <ayushs5@illinois.edu>
2021-11-10 18:04:44 -0600

    Added Game page to webapp

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var difficulty = 1
+
+document.getElementById("start").onclick = start_game();
+document.getElementById("difficulty").onclick = change_difficulty();
+// document.getElementById("ghostmode").onclick = ghost_mode();
+
+function start_game() {
+    console.log("Game Started");
+}
+
+function change_difficulty() {
+    console.log("change difficulty");
+}
+function ghost_mode() {
+    console.log("toggle ghost mode");
+}
