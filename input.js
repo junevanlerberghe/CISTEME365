be9af3c5dd0c96a3c5ccb2d47ae64bf2c9b52903
June Vanlerberghe <junevanlerberghe@junes-mbp-2.lan>
2021-11-13 19:03:16 -0600

    adding moving ship to game

---
+++ input.js
@@ -0,0 +1,30 @@
+import Ship from "./ship.js"
+
+export default class InputHandler {
+
+    constructor(ship) {
+        document.addEventListener("keydown", event => {
+            switch(event.key) {
+                case 'ArrowUp':
+                    ship.moveUp()
+                    break
+
+                case 'ArrowDown':
+                    ship.moveDown()
+                    break
+            }
+        });
+
+        document.addEventListener("keyup", event => {
+            switch(event.key) {
+                case 'ArrowDown':
+                    if(ship.speed > 0) ship.stop()
+                    break
+                    
+                case 'ArrowUp':
+                    if(ship.speed < 0) ship.stop()
+                    break
+            }
+        });
+    }
+}
