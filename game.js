35ac9cc9faef2e0526130f848b154bdf1a5ddeae
Ayush Sharma <41262335+AyushSharma173@users.noreply.github.com>
2021-11-14 21:52:40 -0600

    Added moving random icbergs

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 import Ship from './ship.js'
 import InputHandler from './input.js'
+import glacier from './glacier.js';
 
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
@@ -10,23 +11,28 @@ const GAME_HEIGHT = 600;
 ctx.clearRect(0, 0, 800, 600)
 
 let ship = new Ship(GAME_WIDTH, GAME_HEIGHT)
-
+let glaciers = new glacier(GAME_WIDTH, GAME_HEIGHT);
 new InputHandler(ship);
 
 ship.draw(ctx)
 
 let lastTime = 0
 
+ctx.fillStyle = '#0ff'
+ctx.fillRect(100, 100, 200, 200);
 function gameLoop(timeStamp) {
     let dt = timeStamp - lastTime;
     lastTime = timeStamp
-
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     ship.update(dt);
     ship.draw(ctx);
-
+    glaciers.update(dt);
+    glaciers.draw(ctx);
     requestAnimationFrame(gameLoop)
 }
 
 gameLoop();
+ctx.fillStyle = '#0ff'
+ctx.fillRect(100, 100, 200, 200);
+
 
---
+++ glacier.js
@@ -0,0 +1,33 @@
+export default class glacier {
+    constructor(gameWidth, gameHeight) {
+        const img = new Image();
+            img.src = "iceberg.png";
+
+        this.image = img;
+        this.gameHeight = gameHeight
+        this.gameWidth = gameWidth
+        this.width = 50
+        this.height = 50
+        this.position = {
+            x:  50,
+            y: 200
+        }
+        this.maxSpeed = 5;
+        this.speed = 10;
+    }
+    update(dt) {
+        if(!dt) return;
+        this.position.x -= this.speed;
+        if (this.position.x < 0) {
+            this.position.x = this.gameWidth + 10;
+            this.position.y = Math.random() * (this.gameHeight);
+        }
+    }
+    draw(ctx) {
+        // ctx.fillStyle = '#0ff'
+        // ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+        ctx.drawImage(this.image, this.position.x, this.position.y, 100, 100);
+
+    }
+}
