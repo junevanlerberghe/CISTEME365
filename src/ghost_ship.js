4a57fff1f3c00ec42a3aff3ee4685e5b9d7dc819
June Vanlerberghe <junevanlerberghe@gmail.com>
2021-12-11 13:00:21 -0600

    tuning PID constants

--- src/ghost_ship.js
+++ src/ghost_ship.js
@@ -45,14 +45,14 @@ export default class GhostShip {
         let glacier1_position = this.game.glacier_pair.position1;
         let glacier2_position = this.game.glacier_pair.position2;
 
-        let target_pos_x = glacier1_position.x;
-        let target_pos_y = (glacier1_position.y + glacier2_position.y)/2;
-        let curr_x = this.game.ghost_ship.position.x;
-        let curr_y = this.game.ghost_ship.position.y;
+        let target_pos_y = (glacier1_position.y + glacier2_position.y + this.game.glacier_pair.height/2)/2;
+        //this isn't working, curr_y is NaN
+        //let curr_y = this.game.ghost_ship.position.y;
+        let curr_y = this.position.y
 
-        let Kp = 0.015;
-        let Ki = 0.004;
-        let Kd = 0.3;
+        let Kp = 0.000005;
+        let Ki = 0.0000001;
+        let Kd = 0.0005;
 
         let curr_err = target_pos_y - curr_y
         this.errors.push(curr_err);
@@ -66,22 +66,25 @@ export default class GhostShip {
         let ui = Ki*sum_error
         let ud = Kd*(curr_err - this.errors.at(-2))
         let output = up + ui + ud
-
-        if(this.position.y > target_pos_y){
+        /*
+        if(this.position.y < target_pos_y){
             output = 1*output;
         }
-        console.log(output);
-        console.log("y: " + this.position.y + ", v: " + this.velocity + ", a: " + this.acceleration);
+        */
+        console.log(curr_y);
+        //console.log("y: " + this.position.y + ", v: " + this.velocity + ", a: " + this.acceleration);
         
         // attempted acceleration-based change
+        //i'm confused by this, can we make the code a little more clear?
+        
         this.acceleration += (output > 0  ? (this.acceleration < this.maxAcceleration ? this.deltaAcceleration : 0)
                                                  : (this.acceleration > -this.maxAcceleration ? -this.deltaAcceleration : 0));
         this.velocity += (this.acceleration > 0  ? (this.velocity < this.maxSpeed ? this.acceleration : 0)
                                                  : (this.velocity > -this.maxSpeed ? this.acceleration : 0));
         this.position.y += this.velocity + this.wind.currentVelocity;
-
+        
         // og code
-        // this.position.y += (output + this.speed + this.wind.currentVelocity)//this.wind.currentVelocity + output);
+        //this.position.y += (output + this.speed + this.wind.currentVelocity)//this.wind.currentVelocity + output);
 
         if(this.position.y < 0) this.position.y = 0;
         if(this.position.y + this.height > this.gameHeight) this.position.y = this.gameHeight - this.height;
