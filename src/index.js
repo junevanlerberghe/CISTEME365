825a9b0a071727ed310c98dac8386f4b3594141e
Jon G <coderspriter@gmail.com>
2022-02-14 17:19:15 -0600

    made custom PID input dependent on game mode

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ var difficulty = 1
 
 document.getElementById("start").onclick = start_game;
 document.getElementById("difficulty").onclick = change_difficulty;
-document.getElementById("playerType").onclick = ghost_mode;
-document.getElementById("customPIDCoefficientsCheckbox").onclick = showDiv;
+document.getElementById("playerType").onclick = changePlayerType;
+document.getElementById("customPIDCoefficientsCheckbox").onclick = showCustomCoeffBoxes;
 
 function start_game() {
     console.log("Game Started");
@@ -21,16 +21,20 @@ function start_game() {
 function change_difficulty() {
     console.log("change difficulty: " + document.getElementById("difficulty").selectedIndex);
 }
-function ghost_mode() {
+function changePlayerType() {
     console.log("change player type: " + document.getElementById("playerType").selectedIndex);
+    showCustomCoeffBoxes();
 }
 
 // custom PID coefficient input stuff
 
 // displays PID coefficient inputs for when option selected
 // from https://stackoverflow.com/questions/16015933/how-can-i-show-a-hidden-div-when-a-select-option-is-selected
-function showDiv() {
-    document.getElementById("customPIDCoefficientsInput").style.display = document.getElementById("customPIDCoefficientsCheckbox").checked ? 'flex' : 'none';
+function showCustomCoeffBoxes() {
+    let customPIDCoeffChecked = document.getElementById("customPIDCoefficientsCheckbox").checked;
+    let playerTypeIncludingPID = document.getElementById("playerType").selectedIndex != "0";
+    document.getElementById("customPIDCoefficients").style.display = playerTypeIncludingPID ? 'block' : 'none';
+    document.getElementById("customPIDCoefficientsInput").style.display = customPIDCoeffChecked && playerTypeIncludingPID ? 'flex' : 'none';
 }
 // event listeners and funcs to ensure custom PID coefficient inputs are legal
 document.getElementById("customPIDCoefficientsInput").addEventListener("change", function() {
