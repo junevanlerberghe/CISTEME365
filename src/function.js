77aa78b8704e89e8c2c0f29412f9101d238dc445
jason888kim <55762580+jason888kim@users.noreply.github.com>
2022-03-18 21:20:24 -0500

    attempted moving index to game

---
+++ src/function.js
@@ -0,0 +1,6 @@
+import Game from "./game";
+
+function resumeGame(){
+    Game.resumeGame
+}// I tried a lot of things to make everything go to the game page, but did it didn't work out. Most of it was
+//deleted and reverted back to the original version.
