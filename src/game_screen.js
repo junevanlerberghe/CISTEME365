13a0790c67fb57dcb8876ba2491af02cec13c0d0
June Vanlerberghe <junevanlerberghe@gmail.com>
2022-05-16 18:24:16 -0700

    debugging frame speed

--- src/game_screen.js
+++ src/game_screen.js
@@ -41,6 +41,7 @@ game.start();
 let lastTime = 0
 function gameLoop(timeStamp) {
     let dt = timeStamp - lastTime;
+    console.log('dt', dt)
     game.input(dt);
     lastTime = timeStamp;
     game.input(dt);
