fb1fbc84a13d14cb32bbaee341f4a78437f89474
June Vanlerberghe <junevanlerberghe@gmail.com>
2022-06-21 18:22:21 -0700

    test3

--- src/input.js
+++ src/input.js
@@ -52,6 +52,59 @@ export default class InputHandler {
 
             }
         });
+
+        document.addEventListener("touchstart", startTouch, false);
+        document.addEventListener("touchmove", moveTouch, false);
+                    
+        // Swipe Up / Down / Left / Right
+        var initialX = null;
+        var initialY = null;
+        
+        function startTouch(e) {
+            initialX = e.touches[0].clientX;
+            initialY = e.touches[0].clientY;
+        };
+        
+        function moveTouch(e) {
+            if (initialX === null) {
+            return;
+            }
+        
+            if (initialY === null) {
+            return;
+            }
+        
+            var currentX = e.touches[0].clientX;
+            var currentY = e.touches[0].clientY;
+        
+            var diffX = initialX - currentX;
+            var diffY = initialY - currentY;
+        
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+            // sliding horizontally
+            if (diffX > 0) {
+                // swiped left
+                console.log("swiped left");
+            } else {
+                // swiped right
+                console.log("swiped right");
+            }  
+            } else {
+            // sliding vertically
+            if (diffY > 0) {
+                // swiped up
+                ship.moveUp()
+            } else {
+                // swiped down
+                ship.moveDown()
+            }  
+            }
+        
+            initialX = null;
+            initialY = null;
+            
+            e.preventDefault();
+        };
         /*document.addEventListener("keydown", function(e){
             if (e.key === "Enter"){
                 toggleFullScreen();
